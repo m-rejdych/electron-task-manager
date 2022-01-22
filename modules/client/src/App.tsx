0d9499d42fb4fa6cf500955672b789be81b9cbb8
m-rejdych <rejdych.m.r@gmail.com>
2022-01-22 20:57:00 +0100

    Added database connection.

--- modules/client/src/App.tsx
+++ modules/client/src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.css';
 
 import Container from './components/Container';
 import Board from './components/Board';
 
 const App: React.FC = () => {
-  console.log('Hello!');
+  useEffect(() => {
+    fetch('http://localhost:8080/').then((res) => res.json()).then(data => console.log(data)).catch(err => console.log(err.message));
+  }, []);
 
   return (
     <Container>
--- modules/server/src/index.ts
+++ modules/server/src/index.ts
@@ -1,14 +1,43 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import { createConnection } from 'typeorm';
 
 dotenv.config();
 
-const app = express();
+const main = async () => {
+  try {
+  const app = express();
 
-app.get('/', (_, res) => {
-  res.json({ message: 'heelo!' });
-})
+  await createConnection({
+    username: process.env.DB_USERNAME as string,
+    port: parseInt(process.env.DB_PORT as string),
+    database: process.env.DB_NAME as string,
+    password: process.env.DB_PASSWORD as string,
+    host: process.env.DB_HOST as string,
+    synchronize: true,
+    logging: true,
+    type: 'postgres',
+    entities: ["./entity/*.ts"],
+  });
 
-app.listen(process.env.PORT, () => {
-  console.log(`App is running on http://localhost:${process.env.PORT}`);
-})
+  app.use(helmet());
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get('/', async (_, res) => {
+    res.json({ message: 'heelo!' });
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`App is running on http://localhost:${process.env.PORT}`);
+  });
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+};
+
+main();
